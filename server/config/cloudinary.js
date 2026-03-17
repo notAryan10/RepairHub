e419583fc830c9afa36552c0f07df0530c44ef64
Aryan Verma <aryanverma1857@gmail.com>
2026-03-17 11:57:48 +0530

    Remove console log for Cloudinary config
    Removed console log for sensitive Cloudinary credentials.

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,12 +1,6 @@
 const { v2: cloudinary } = require('cloudinary');
 require('dotenv').config();
 
-console.log('Cloudinary env loaded:', {
-    cloud_name: process.env.CLOUD_NAME,
-    api_key_set: !!process.env.CLOUD_API_KEY,
-    api_secret_set: !!process.env.CLOUD_API_SECRET,
-});
-
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
